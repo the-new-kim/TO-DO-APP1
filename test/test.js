1c9000aad9f5343324249b7eba04a2a441b7b2c3
the-new-kim <jonkim0309@gmail.com>
2021-05-15 02:05:16 +0900

    123

--- test/test.js
+++ test/test.js
@@ -6,14 +6,34 @@ const rightBtn = document.querySelector(".rightBtn");
 const boxDivs = document.querySelectorAll(".box");
 
 let boxes = [];
+let newNumber = 1;
 
-function handleLeftBtn(event) {}
+function handleRightBtn(event) {
+  if (newNumber >= boxes.length) {
+    console.log("too big");
+  } else {
+    console.log((newNumber += 1));
+  }
+}
+
+function handleLeftBtn(event) {
+  newNumber--;
+  const lastBox = boxes[boxes.length - 1];
+  let currentBox = boxes[newNumber];
+  if (newNumber <= 1) {
+    currentBox = lastBox;
+    console.log(currentBox);
+  } else {
+    console.log((newNumber -= 1));
+  }
+}
 
 function loadBox() {
   boxes = Array.from(boxDivs);
-  currentBox = boxes[0];
+  boxes[boxes.length - 1].innerText = newNumber;
 
   leftBtn.addEventListener("click", handleLeftBtn);
+  rightBtn.addEventListener("click", handleRightBtn);
 }
 
 function init() {
