064001201ac1db4ad1d752dd7723491258b91d00
the-new-kim <jonkim0309@gmail.com>
2021-05-16 02:15:03 +0900

    asdf

--- test/test.js
+++ test/test.js
@@ -3,24 +3,56 @@
 const prevBtn = document.querySelector(".prevBtn");
 const nextBtn = document.querySelector(".nextBtn");
 
-const Divcontainers = document.querySelectorAll(".container");
-
-const containers = Array.from(Divcontainers);
 let currentNumber = 0;
 
-function handleprevBtn(event) {
-  console.log("hi");
-  currentNumber -= 1;
+const SHOWING_CL = "showing";
+const HIDING_CL = "hiding";
 
-  if (currentNumber < 0) {
+function displaySlide(number) {
+  const DivContainers = document.querySelectorAll(".container");
+  const containers = Array.from(DivContainers);
+
+  if (number > containers.length) {
+    currentNumber = 0;
+  }
+  if (number < 0) {
     currentNumber = containers.length - 1;
   }
-  console.log(containers[currentNumber]);
+  for (let i = 0; i < containers.length; i++) {
+    containers[i].classList.add(HIDING_CL);
+  }
+  containers[currentNumber].classList.remove(HIDING_CL);
 }
 
 function init() {
-  console.log(prevBtn);
-  prevBtn.addEventListener("click", handleprevBtn);
+  displaySlide(currentNumber);
 }
 
 init();
+
+/*
+ <script type="text/javascript">  
+        var slide_index = 1;  
+        displaySlides(slide_index);  
+  
+        function nextSlide(n) {  
+            displaySlides(slide_index += n);  
+        }  
+  
+        function currentSlide(n) {  
+            displaySlides(slide_index = n);  
+        }  
+  
+        function displaySlides(n) {  
+            var i;  
+            var slides = document.getElementsByClassName("showSlide");  
+            if (n > slides.length) { slide_index = 1 }  
+            if (n < 1) { slide_index = slides.length }  
+            for (i = 0; i < slides.length; i++) {  
+                slides[i].style.display = "none";  
+            }  
+            slides[slide_index - 1].style.display = "block";  
+        }  
+    </script>  
+
+*/
