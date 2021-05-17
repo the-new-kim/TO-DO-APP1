e14a2f737b845483a2718413d125b69ca5e5bf02
the-new-kim <jonkim0309@gmail.com>
2021-05-17 22:33:19 +0900

    fdgh

--- addlist.js
+++ addlist.js
@@ -17,10 +17,50 @@ function addNewList() {
 
 function paintLists(text) {
   const wrapperDiv = document.createElement("div");
+
+  const mainDiv = document.createElement("div");
+
+  const greetingContainer = document.createElement("div");
+  const greetingForm = document.createElement("form");
+  const greetingInput = document.createElement("input");
+
+  const todoContainer = document.createElement("div");
+  const todoForm = document.createElement("form");
+  const todoInput = document.createElement("input");
+
   const listId = lists.length + 1;
+
+  mainDiv.classList.add("main");
+
+  greetingContainer.classList.add("main__greeting");
+  greetingForm.classList.add("js-greetingForm");
+  greetingInput.type = "text";
+  greetingInput.placeholder = "Enter name";
+  greetingInput.required = true;
+
+  greetingForm.appendChild(greetingInput);
+  greetingContainer.appendChild(greetingForm);
+
+  todoContainer.classList.add("main__todo");
+  todoForm.classList.add("js-todoForm");
+  todoInput.type = "text";
+  todoInput.placeholder = "to do";
+  todoInput.minLength = 1;
+  todoInput.maxLength = 70;
+  todoInput.required = true;
+
+  todoForm.appendChild(todoInput);
+  todoContainer.appendChild(todoForm);
+
+  mainDiv.appendChild(greetingContainer);
+  mainDiv.appendChild(todoContainer);
+
+  wrapperDiv.appendChild(mainDiv);
+
   wrapperDiv.id = `js-wrapper${listId}`;
   wrapperDiv.classList.add("wrapper");
   document.body.appendChild(wrapperDiv);
+
   const newListObj = {
     name: text,
     id: listId,
